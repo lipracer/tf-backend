856ed196c8569334f7ce9d8177ae2c73932d85b6
lipracer <lipracer@gmail.com>
2023-12-01 22:51:24 +0800

    [Feature] op registry codegn add option step 1

--- ops/native/native_ops.h
+++ ops/native/native_ops.h
@@ -6,7 +6,8 @@ namespace tfbe
 {
 namespace autogen
 {
-Tensor AddV2(const Tensor& lhs, const Tensor& rhs);
+Tensor AddVN(const Tensor& lhs, const Tensor& rhs);
+
 Tensor AddV3(const Tensor& lhs, const Tensor& rhs, float alpha);
 }
 } // namespace tfbe
